0556eefa7684e16cdc4dc10011c12b54e356f19d
이경훈 <rudgnsdleh@naver.com>
2025-05-14 17:22:17 +0900

    day2-3

--- app/page.tsx
+++ app/page.tsx
@@ -2,46 +2,40 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main
-      className="bg-gray-100 sm:bg-red-200 h-screen md:bg-green-200 
-      lg:bg-blue-200 xl:bg-orange-100 2xl:bg-purple-100  
-    flex items-center justify-center p-5"
-      //테일윈드는 소형기기를 메인으로 잡는다 그리고 각 크기마다 지정한 값을 줄수있다
-    >
+    <main className="h-screen bg-gray-100 flex items-center justify-center p-5">
       <div
         className="bg-white shadow-lg p-5 rounded-3xl w-full 
-      max-w-screen-sm flex md:flex-row flex-col gap-5 *:outline-none
-      ring ring-transparent transition-shadow
-      has-[:invalid]:bg-red-100 has-[:invalid]"
-        //*를 사용하면하위에 있는 자식 컨테이너들은 기본 값으로 적용됌
-        //has를 사용하게되면 hsa뒤에 있는 값이 있는 하위 자식 컨테이너들에게 전부 적용
+      max-w-screen-sm flex flex-col gap-3"
       >
-        <input
-          className="w-full rounded-full h-10 
-          ring-3 ring-transparent focus:ring-green-500 focus:ring-offset-2
-          transition-shadow
-          placeholder:drop-shadow invalid:focus:ring-red-200 peer"
-          //여기서 h로 값을 지정할수있지만 또 py로 위아래 패딩값을 줘 크기를 지정할수있음
-          //invalid값도 따로 지정해 설정할수있어서 타입값이 다르면 required을 사용해 invalid로 경고성 색상을 출력가능
-          //또한 invalid 뒤에 focus등을 넣어 추가로 기능을 활성화 할수있음
-          //peer는 형제관계처럼 연결해줄수있음
-          type="email"
-          required
-          placeholder="이메일을 입력하시오"
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block">
-          e-mail 값 입력은 필수입니다
-        </span>
-        <button
-          className="text-white py-4 rounded-2xl 
-          focus:scale-90 md:w-20
-          bg-gray-600/50
-          peer-required::bg-green-500
-          "
-          //bg-gradient-to-tr from-cyan-500 via-red-300 to-purple-400 그라데이션 주는 방법
-        >
-          로그인
-        </button>
+        {["2B", "Me", "You", "Yourself"].map((person, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-5 group
+          " //odd는 홀수 위치에 even 은 짝수 예)odd:bg-gray-200 even:bg-cyan-100
+            //last는 마지막값을 지정해 줄수있음 반대로 first로 하면 첫번째 값 예)last:border-0
+          >
+            <div className="size-10 bg-blue-400 rounded-full" />
+            <span
+              className="text-lg font-medium group-hover:text-red-500
+            empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300"
+              //empty로 값이 비워져 있을때 상태를 따로 설정해줄수 있음
+              //group은 부모에 주고 그 아래 자식 에 group을 이용해 각 상태에 값을 지정해줄수 있음
+              //px 값으로 설정하고 싶다면 예)h-[101.569px] 이런식으로 입력 가능
+              //색상값도 bg[#7d45b4] 이런식으로 값을 주면 됌
+            > 
+              {person}
+            </span>
+            <div
+              className="size-6  bg-red-500 text-white flex 
+            items-center justify-center rounded-full relative"
+              //animate을 줄수있음 씨바꺼 존나게 간단하네 예) animate-bounce
+              //그리고 스켈레톤을 만들때 animate-pulse 쓰면 깜빡이는 효과를 한번에 끝!!!!
+            >
+              <span className="z-10">{index}</span>
+              <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
