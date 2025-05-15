2c8b6a71694610d3fbd4034f44a35ce7ead5e8f6
이경훈 <rudgnsdleh@naver.com>
2025-05-15 21:26:35 +0900

    day3-9 zod 에러메시지 커스텀

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -3,12 +3,49 @@ import { z } from "zod";
 
 const usernameSchema = z.string().min(5).max(10);
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  comfirm_Password: z.string().min(10),
-});
+const checkUesrname = (uesrname: string) => !uesrname.includes("potato");
+const checkPassword = ({
+  password,
+  comfirm_Password,
+}: {
+  password: string;
+  comfirm_Password: string;
+}) => password === comfirm_Password;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "문자로만 가능해요!",
+        required_error: "닉네임을 입력해주세요",
+      })
+      .min(3, "닉네임이 너무 짧아요~")
+      .max(10, "닉네임이 너무 길어요~~~~~")
+      .refine(checkUesrname, "potato는 안되요~"),
+
+    email: z
+      .string({
+        invalid_type_error: "이메일을 입력해주세요",
+        required_error: "이메일을 입력해주세요",
+      })
+      .email("이메일을 입력해주세요"),
+    password: z
+      .string({
+        invalid_type_error: "특수문자는 불가능해요!",
+        required_error: "비밀번호를 입력해주세요",
+      })
+      .min(10, "비밀번호가 너무 짧아요"),
+    comfirm_Password: z
+      .string({
+        invalid_type_error: "특수문자는 불가능해요!",
+        required_error: "비밀번호를 입력해주세요",
+      })
+      .min(10, "비밀번호가 너무 짧아요"),
+  })
+  .refine(checkPassword, {
+    message: "비밀번호 틀렸습니다, 다시 한번 확인해주세요.",
+    path: ["comfirm_Password"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
