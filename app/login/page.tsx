cefa6ff1d8d64db9d90cb157f8d7a6401a4df37f
이경훈 <rudgnsdleh@naver.com>
2025-05-15 14:53:19 +0900

    day3-2

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,25 +2,19 @@ import FormInput from "@/components/form-input";
 import FormBtn from "@/components/from-btn";
 import SocialLogin from "@/components/social-login";
 
-export default function CreateAccount() {
+export default function LogIn() {
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
-        <h2 className="text-xl">당근당근 가입 당근합니다!</h2>
+        <h2 className="text-xl">로그인 시 이메일과 비밀번호를 입력하세요</h2>
       </div>
       <form className="flex flex-col gap-3">
-        <FormInput
-          type="text"
-          placeholder="사용자 이름"
-          required
-          errors={["사용자 이름이 너무 짧습니다"]}
-        />
         <FormInput
           type="email"
           placeholder="e-mail"
           required
-          errors={["email 형식이 아닙니다다"]}
+          errors={["email 형식이 아닙니다"]}
         />
         <FormInput
           type="password"
@@ -28,15 +22,10 @@ export default function CreateAccount() {
           required
           errors={["비밀번호가 너무 짧습니다"]}
         />
-        <FormInput
-          type="password"
-          placeholder="비밀번호 확인"
-          required
-          errors={["비밀번호가 틀립니다"]}
-        />
-        <FormBtn loading={false} text={"가입 완료"} />
+
+        <FormBtn loading={false} text={"로그인"} />
       </form>
-      <SocialLogin text={" 가입하기"} />
+      <SocialLogin text={" 로그인"} />
     </div>
   );
 }
---
+++ app/sms/page.tsx
@@ -0,0 +1,29 @@
+import FormInput from "@/components/form-input";
+import FormBtn from "@/components/from-btn";
+
+export default function SMSLogIn() {
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">SMS 로그인</h1>
+        <h2 className="text-xl">핸드폰 번호를 입력하세요</h2>
+      </div>
+      <form className="flex flex-col gap-3">
+        <FormInput
+          type="number"
+          placeholder="번호를 입력하세요"
+          required
+          errors={["email 형식이 아닙니다"]}
+        />
+        <FormInput
+          type="number"
+          placeholder="인증번호를 입력하세요"
+          required
+          errors={["비밀번호가 너무 짧습니다"]}
+        />
+
+        <FormBtn loading={false} text={"인증번호 확인"} />
+      </form>
+    </div>
+  );
+}
