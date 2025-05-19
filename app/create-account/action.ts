67952f9ba02a0ea84d3468c9c81947fe47ca7d10
이경훈 <rudgnsdleh@naver.com>
2025-05-19 15:08:19 +0900

    day6-2 비번번 hash처리 하기

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -1,4 +1,5 @@
 "use server";
+import bcrypt from "bcrypt";
 import { PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constants";
 import db from "@/lib/db";
 import { boolean, z } from "zod";
@@ -88,5 +89,17 @@ export async function createAccount(prevState: any, formData: FormData) {
   if (!result.success) {
     return result.error.flatten();
   } else {
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+    console.log(user);
   }
 }
