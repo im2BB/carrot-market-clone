2f79042eff0d0011e4c4dffcf3b47c05bd6b67c1
이경훈 <rudgnsdleh@naver.com>
2025-05-23 21:15:52 +0900

    day9-5 게시물 추가 페이지 생성성

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,7 +1,8 @@
-import ListProducts from "@/components/list-products";
 import ProductList from "@/components/product-list";
 import db from "@/lib/db";
 import { Prisma } from "@/lib/generated/prisma";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
 async function getInitialProducts() {
   const products = await db.product.findMany({
@@ -29,6 +30,15 @@ export default async function Products() {
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
+      <Link
+        href="/products/add"
+        className="bg-orange-500 flex items-center
+      justify-center rounded-full size-12 fixed 
+      bottom-24 right-8 text-white transition-colors 
+      hover:bg-orange-400"
+      >
+        <PlusIcon className="size-8" />
+      </Link>
     </div>
   );
 }
