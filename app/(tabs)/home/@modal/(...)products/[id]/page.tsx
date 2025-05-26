162fdc78cf5db9605b2679531977654147fd16f4
이경훈 <rudgnsdleh@naver.com>
2025-05-26 18:20:57 +0900

    day10-6 모달 페이지 닫기버튼  컴포넌트화

--- app/(tabs)/home/@modal/(...)products/[id]/page.tsx
+++ app/(tabs)/home/@modal/(...)products/[id]/page.tsx
@@ -1,25 +1,13 @@
-"use client";
-
+import CloseButton from "@/components/closeButton";
 import { PhotoIcon } from "@heroicons/react/16/solid";
-import { XMarkIcon } from "@heroicons/react/24/solid";
-import { useRouter } from "next/navigation";
 
 export default function Modal({ params }: { params: { id: string } }) {
-  const router = useRouter();
-  const onCloseClick = () => {
-    router.back();
-  };
   return (
     <div
       className=" absolute w-full h-full flex z-50 justify-center 
     items-center bg-black bg-opacity-60 top-0 left-0"
     >
-      <button
-        onClick={onCloseClick}
-        className=" absolute right-20 top-5 text-neutral-200"
-      >
-        <XMarkIcon className="size-10" />
-      </button>
+      <CloseButton />
       <div className="max-w-screen-sm h-1/2 w-full flex justify-center">
         <div
           className="aspect-square bg-neutral-700 rounded-md text-neutral-200
