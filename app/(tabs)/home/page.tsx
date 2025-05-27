345430b2840a0bac25c2f00802363772cf2818cc
이경훈 <rudgnsdleh@naver.com>
2025-05-27 21:44:11 +0900

    day11-4 revalidatePath로 캐쉬 설정

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -2,11 +2,11 @@ import ProductList from "@/components/product-list";
 import db from "@/lib/db";
 import { Prisma } from "@/lib/generated/prisma";
 import { PlusIcon } from "@heroicons/react/24/solid";
-import { unstable_cache as nextCache } from "next/cache";
+import { unstable_cache as nextCache, revalidatePath } from "next/cache";
 import Link from "next/link";
 
 const getCachedProducts = nextCache(getInitialProducts, ["home-products"], {
-  revalidate: 60, //60초 마다가 아닌 유저가 들어온후 60초 후 새로고침을 하게되면 새로운 캐쉬를 받음
+  //revalidate: 60, 60초 마다가 아닌 유저가 들어온후 60초 후 새로고침을 하게되면 새로운 캐쉬를 받음
 });
 
 export const metadata = {
@@ -36,9 +36,16 @@ export type InitialProducts = Prisma.PromiseReturnType<
 
 export default async function Products() {
   const initialProducts = await getCachedProducts();
+  const revalidate = async () => {
+    "use server";
+    revalidatePath("/home");
+  };
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
+      <form action={revalidate}>
+        <button>검증</button>
+      </form>
       <Link
         href="/add-products"
         className="bg-orange-500 flex items-center
