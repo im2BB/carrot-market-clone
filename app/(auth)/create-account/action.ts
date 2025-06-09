83f3e343f2d0deffbbb0e25860dc8d4257d8e921
이경훈 <rudgnsdleh@naver.com>
2025-06-09 14:54:23 +0900

    day16-2 가입시 비밀번호 확인 부분 오류 수정

--- app/(auth)/create-account/action.ts
+++ app/(auth)/create-account/action.ts
@@ -5,97 +5,38 @@ import { z } from "zod";
 import { redirect } from "next/navigation";
 import getSession from "@/lib/seeeion";
 
-const usernameSchema = z.string().min(5).max(10);
-
-const checkUesrname = (uesrname: string) => !uesrname.includes("potato");
-const checkPasswords = ({
-  password,
-  confirm_password,
-}: {
-  password: string;
-  confirm_password: string;
-}) => password === confirm_password;
-
 const formSchema = z
   .object({
     username: z
-      .string({
-        invalid_type_error: "문자로만 가능해요!",
-        required_error: "닉네임을 입력해주세요",
-      })
+      .string()
+      .min(3, "닉네임은 3자 이상이어야 합니다")
+      .max(10, "닉네임은 10자 이하여야 합니다"),
 
-      .toLowerCase() //소문자로 변환
-      .trim() //공백제거
-      // .transform((username) => {
-      //   return '* {username} *'
-      // })   이런식으로 변경해서 값을 보낼수 있음
-      .refine(checkUesrname, "potato는 안되요~"),
+    email: z.string().email("올바른 이메일 형식이 아닙니다"),
 
-    email: z
-      .string({
-        invalid_type_error: "이메일을 입력해주세요",
-        required_error: "이메일을 입력해주세요",
-      })
-      .toLowerCase()
-      .email(),
+    password: z.string().min(4, "비밀번호는 4자 이상이어야 합니다"),
 
-    password: z.string().min(4, "비밀번호가 너무 짧아요"),
-
-    //   .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
-    // comfirm_Password: z.string().min(10, "비밀번호가 너무 짧아요"),
+    confirm_password: z.string().min(4, "비밀번호는 4자 이상이어야 합니다"),
   })
-  .superRefine(async ({ username }, ctx) => {
-    const user = await db.user.findUnique({
-      where: {
-        username,
-      },
-    });
-    if (user) {
-      ctx.addIssue({
-        code: "custom",
-        message: "이미 사용중인 이름입니다.",
-        path: ["username"],
-        fatal: true,
-      });
-      return z.NEVER;
-    }
-  })
-  .superRefine(async ({ email }, ctx) => {
-    const user = await db.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (user) {
-      ctx.addIssue({
-        code: "custom",
-        message: "이미 사용중인 이메일 입니다.",
-        path: ["email"],
-        fatal: true,
-      });
-      return z.NEVER;
-    }
-  })
-  .refine(checkPasswords, {
-    message: "비밀번호가 다릅니다!",
+  .refine((data) => data.password === data.confirm_password, {
+    message: "비밀번호가 일치하지 않습니다",
     path: ["confirm_password"],
   });
 
 export async function createAccount(prevState: any, formData: FormData) {
-  const data = {
-    username: formData.get("username") ?? "",
-    email: formData.get("email") ?? "",
+  const values = {
+    username: formData.get("username"),
+    email: formData.get("email"),
     password: formData.get("password"),
-    comfirm_Password: formData.get("comfirm_Password"),
+    confirm_password: formData.get("confirm_password"),
   };
-  const result = await formSchema.spa(data);
+
+  const result = await formSchema.safeParseAsync(values);
+
   if (!result.success) {
     return {
-      ...result.error.flatten(),
-      values: {
-        username: data.username,
-        email: data.email,
-      },
+      fieldErrors: result.error.flatten().fieldErrors,
+      values,
     };
   } else {
     const hashedPassword = await bcrypt.hash(result.data.password, 12);
