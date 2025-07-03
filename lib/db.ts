1c608c8881b7c43036c9f1979568e30dc7e8e245
이경훈 <rudgnsdleh@naver.com>
2025-07-03 17:11:13 +0900

    타입 오류 수정-7

--- lib/db.ts
+++ lib/db.ts
@@ -2,20 +2,4 @@ import { PrismaClient } from "./generated/prisma";
 
 const db = new PrismaClient();
 
-async function test() {
-  const token = await db.sMSToken.create({
-    data: {
-      token: "121212",
-      user: {
-        connect: {
-          id: 1,
-        },
-      },
-    },
-  });
-  console.log(token);
-}
-
-test();
-
 export default db;
