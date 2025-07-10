111177e6fbe5c67f43315dac7ffaf78159ac7717
이경훈 <rudgnsdleh@naver.com>
2025-07-10 13:03:49 +0900

    new build -2.2.4

--- next.config.js
+++ next.config.js
@@ -3,6 +3,31 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     domains: [
       "imagedelivery.net",
